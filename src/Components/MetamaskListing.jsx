3a9255091f26e49f91ec446c56fcda92f8e1d8eb
sheri6303 <ahmedsheraz124@gmail.com>
2022-07-18 17:22:23 +0500

    changes

--- src/Components/MetamaskListing.jsx
+++ src/Components/MetamaskListing.jsx
@@ -18,6 +18,7 @@ export default function MetamaskListing() {
   function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
+        //hello
         var temp = array[i];
         array[i] = array[j];
         array[j] = temp;
