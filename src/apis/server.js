58ffdbd3ad642f3eed18e072f6c2ce08618d272a
Programmers Cube <ahsanshaikh8@gmail.com>
2023-03-30 11:08:40 +0500

    nft frontend

--- src/apis/server.js
+++ src/apis/server.js
@@ -4,8 +4,8 @@ import axios from "axios";
 
 //export const baseUrl = "http://51.222.241.160:8001/api/";
  
- //export const baseUrl = "http://194.5.193.238:8000/api/";
- export const baseUrl="http://localhost:8000/api/";
+ export const baseUrl = "https://immense-savannah-42494.herokuapp.com/api/";
+ //export const baseUrl="http://localhost:8000/api/";
 
 //  export const baseUrl = "https://8e549e6c70b3.ngrok.io";
 export default axios.create({
